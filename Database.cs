bf81a1bf6aaba75a89b9617f7e96232ce32c5d91
Ress <52524247+Ress-sama@users.noreply.github.com>
2020-12-24 02:45:27 +0300

    add database

---
+++ Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketProject
+{
+    class Database
+    {
+        static public List<MehsulStructure> mehsuls = new List<MehsulStructure>();
+        static public void addData()
+        {
+            MehsulStructure mehsul_1 = new MehsulStructure();
+
+            Console.WriteLine("+");
+            mehsul_1.ad = "Winston";
+            mehsul_1.qiymet = 3.50f;
+            mehsul_1.say = 2;
+            mehsul_1.kod = 1;
+            mehsul_1.kateqoriya = MehsulKateqoriya.Siqaret;
+            mehsuls.Add(mehsul_1);
+        }
+
+    }
+    
+}
